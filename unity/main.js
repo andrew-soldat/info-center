709f87d5854e9cceaea6cf29e62d95c452e0ea6a
BlishchAndrew <blishch.andrew@gmail.com>
2021-04-06 12:19:50 +0300

    add block number

--- unity/main.js
+++ unity/main.js
@@ -10,3 +10,33 @@ function parallax (e) {
 }
 
 document.addEventListener('mousemove', parallax);
+
+
+var a = 0;
+   $(window).scroll(function () {
+      var oTop = $(".number__item").offset().top - window.innerHeight;
+      if (a == 0 && $(window).scrollTop() > oTop) {
+         $(".number__count").each(function () {
+            var $this = $(this),
+               countTo = $this.attr("data-count");
+            $({
+               countNum: $this.text(),
+            }).animate(
+               {
+                  countNum: countTo,
+               },
+               {
+                  duration: 800,
+                  easing: "swing",
+                  step: function () {
+                     $this.text(Math.floor(this.countNum));
+                  },
+                  complete: function () {
+                     $this.text(this.countNum);
+                  },
+               }
+            );
+         });
+         a = 1;
+      }
+	});
